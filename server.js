658c8388a650f9ae6aee92776d466a8dcc6c70eb
Richuma <richard.ric738@gmail.com>
2016-10-01 22:36:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,9 +13,46 @@ var articleOne= {
   content: 'This is my first conset page This is my first conset pageThis is my first conset pageThis is my first conset page This is my first conset page This is my first conset page This is my first conset page This is my first conset page '
 };
 
+function createTemplate(data){
+var title = data.title;
+var date  = data.date;
+var heading = data.heading;
+var content = data.content;
 
+var htmltemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viexport" content="width=device=width,intial-sccale-1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+         </head>
+    <body>
+        <div> 
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+        ${date}
+        </div}
+        <div>
+            
+                this is my first web page
+        </div>
+        ${content}
+        <div>
+     </div>
+    </body>
+</html>
+`;
+ return htmlTemplate;
+}
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(aticleOne));
 });
 
 app.get('/article-one', function(eq,res){
